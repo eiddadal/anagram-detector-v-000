9ed884ddee099522e3fc30b513150217c5771d02
Dalya Eidda <dalia.eidda@gmail.com>
2019-01-15 00:09:18 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,6 +2,10 @@
 class Anagram
   attr_accessor :word
   
+  def initialize(word, list)
+    @word = word.sort 
+    list = list.split("").sort 
+    word.match {|word, list| word == list}
  
   
 end 
