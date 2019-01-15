9ad73462330463114f1fb30117ea7a9e626a208d
Dalya Eidda <dalia.eidda@gmail.com>
2019-01-15 00:15:53 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,9 +3,14 @@ class Anagram
   attr_accessor :list
   
   def initialize(word, list)
-    word = word.split("").sort 
+     
     @list = list
-    list = list.sort 
+    
+ end 
+ 
+ def match 
+   word = word.split("").sort
+   @list.sort 
     list.find_all {|i, word| %w(i) == word}
  end 
   
