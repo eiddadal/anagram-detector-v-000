93f62dc38100cf7e68823932d14f43691dc5239c
Dalya Eidda <dalia.eidda@gmail.com>
2019-01-15 00:17:43 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,7 +6,7 @@ class Anagram
     @word = word
  end 
  
- def match (word, word_array)
+ def match (word_array)
    @word = word.split("").sort
    word_array.sort 
   word_array.match{|word| %w(list) == word}
